a62c3dfcdb60a0efdacb958b050af7273bdcde6a
Marcos Palacios <mp2772@gmail.com>
2023-07-27 20:34:11 -0400

    updated jwt example

--- task-list-client/src/App.js
+++ task-list-client/src/App.js
@@ -4,17 +4,20 @@ import NavBar from "./components/NavBar";
 import HomePage from "./components/HomePage";
 import Tasks from "./components/tasks/Tasks";
 import { TaskProvider } from "./contexts/Tasks.context";
+import { UserProvider } from "./contexts/User.context";
 
 function App() {
 	return (
 		<div className="App">
-			<NavBar />
-			<TaskProvider>
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/tasks" element={<Tasks />} />
-				</Routes>
-			</TaskProvider>
+			<UserProvider>
+				<NavBar />
+				<TaskProvider>
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="/tasks" element={<Tasks />} />
+					</Routes>
+				</TaskProvider>
+			</UserProvider>
 		</div>
 	);
 }
--- task-list-client/src/components/NavBar.js
+++ task-list-client/src/components/NavBar.js
@@ -1,11 +1,81 @@
-import React from "react";
+import { useState, useContext } from "react";
 import { NavLink } from "react-router-dom";
 
+import UserContext from "../contexts/User.context";
+import axios from "axios";
+
 export default function NavBar() {
+	const { user, setUser } = useContext(UserContext);
+
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
+
+	const [isSignUp, setisSignUp] = useState(null);
+
+	const handleChange = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	const submitForm = async () => {
+		let urlType = isSignUp ? "signup" : "login";
+		let storedToken = localStorage.getItem("user");
+
+		try {
+			const data = await axios.post(
+				`http://localhost:3000/auth/
+			http://localhost:3000/auth/${urlType}`,
+				formData,
+				{
+					withCredentials: true,
+					headers: {
+						Authorization: `Bearer ${storedToken}`,
+					},
+				}
+			);
+			setUser(() => (isSignUp ? null : data));
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		submitForm();
+	};
+
 	return (
 		<div>
 			<NavLink to="/">Home</NavLink>
 			<NavLink to="/tasks">Tasks</NavLink>
+			<div>
+				<button onClick={() => setisSignUp(true)}>Sign Up</button>
+				<button onClick={() => setisSignUp(false)}>Log In</button>
+				{
+					<div>
+						<label>
+							Email:{" "}
+							<input
+								type="text"
+								name="email"
+								value={formData.email}
+							/>
+						</label>
+						<label>
+							Password:{" "}
+							<input
+								type="password"
+								name="password"
+								value={formData.password}
+							/>
+						</label>
+					</div>
+				}
+			</div>
 		</div>
 	);
 }
